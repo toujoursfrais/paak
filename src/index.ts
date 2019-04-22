d59d4edccc7bfd539c15c240090a0542a13bfc44
Min Nam <hi@minnam.io>
2019-04-22 03:31:08 -0700

    chore: travis ci

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
-import * as array from './validators/array'
+import * as array  from './validators/array'
 import * as common from './validators/common'
-import * as date from './validators/date'
+import * as date   from './validators/date'
 import * as object from './validators/object'
 import * as string from './validators/string'
+import * as n      from './normalizers'
 
-import * as n from './normalizers'
+export default {
+  v: {
+    ...array,
+    ...common,
+    ...date,
+    ...object,
+    ...string,
+  },
+  n,
+  validate: object.validate
+}
 
 module.exports = {
   v: {
