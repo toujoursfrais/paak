80bc850169b8bbfd2d52df9da6c66147a71a4044
Min Nam <hi@minnam.io>
2019-04-22 01:01:02 -0700

    doc: configure for typedoc

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-import validators from './validators'
-import * as normalizers from './normalizers'
+import * as array from './validators/array'
+import * as common from './validators/common'
+import * as date from './validators/date'
+import * as object from './validators/object'
+import * as string from './validators/string'
+
+import * as n from './normalizers'
 
 export default {
-  validators,
-  normalizers
+  v: {
+    array,
+    common,
+    date,
+    object,
+    string,
+  },
+  n,
+  validate: object.validate
 }
